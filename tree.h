3de47ee17ae5cd7d349ec97a30bd3f12257478dd
Paulmahaut <paul.mahaut@gmail.com>
2024-11-13 13:41:22 +0100

    Update tree.h

--- tree.h
+++ tree.h
@@ -3,3 +3,14 @@
 #include "loc.h"
 #include "map.h"
 #include "moves.h"
+
+
+typedef struct s_treeNode {
+    t_localisation loc;
+    int cost;                 // Accumulated cost up to this node
+    struct s_treeNode** children; // Array of pointers to children nodes
+    int num_children;         // Number of children (max 9 for 9 possible moves)
+    int height;              // Height of the node
+    int width;              // Width of the node
+    int used_moves[100];         // Array of moves used to reach this node
+} t_treeNode;
