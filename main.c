f99ab1b91ea64e808f6690fc5b8ceb9c7d97afba
brunraphael <raphael78.brun@gmail.com>
2024-11-13 11:45:15 +0100

    Added test calls

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "map.h"
+#include "tree.h"
+#include "moves.h"
+#include "loc.h"
 
 int main() {
     t_map map;
@@ -32,5 +35,20 @@ int main() {
         printf("\n");
     }
     displayMap(map);
+
+    // Set initial position of MARC
+    t_localisation marc_loc = loc_init(0, 0, NORTH);
+
+    // Create the root of the movement tree with initial cost
+    t_treeNode* root = createNode(marc_loc, map.costs[marc_loc.pos.y][marc_loc.pos.x]);
+
+    // Find the optimal path in terms of cost from root to leaf
+    int min_cost;
+    findMinCostPath(root, &min_cost);
+    printf("Minimum cost to reach target: %d\n", min_cost);
+
+    // Free allocated resources for the tree
+    freeTree(root);
+
     return 0;
 }
