dc729bbe574cc7d36bad8b21529937cfc304aef8
Paulmahaut <paul.mahaut@gmail.com>
2024-11-19 11:20:49 +0100

    Modify functions to adapt them for MARC
    Two errors line 42 and 64

--- tree.c
+++ tree.c
@@ -11,10 +11,36 @@ t_treeNode* createNode(t_move move, int cost) {
     return node;
 }
 
+// Première version de la fonction addChild
+/*void addChild(t_treeNode *parent, t_treeNode *child) {
+    parent->children = realloc(parent->children, (parent->num_children + 1) * sizeof(t_treeNode *));
+    parent->children[parent->num_children] = child;
+    parent->num_children++;
+}*/
+
+// Deuxième version de la fonction addChild
 void addChild(t_treeNode *parent, t_treeNode *child) {
     parent->children = realloc(parent->children, (parent->num_children + 1) * sizeof(t_treeNode *));
     parent->children[parent->num_children] = child;
     parent->num_children++;
+
+    t_localisation loc = parent->loc; // Déclaration de loc
+    t_map map = parent->map; // Déclaration de map
+
+    for (int j = 0; j < 9; j++) { // 9 mouvements possibles
+        t_move move = (t_move) j;
+        int move_cost = calculateMoveCost(loc, move, map);
+
+        if (move_cost == COST_UNDEF) continue; // Ignore les mouvements invalides
+
+        int total_cost = parent->cost + move_cost;
+
+        t_treeNode *child = createNode(move, total_cost);
+        addChild(parent, child);
+
+        // Simule la position de MARC après le mouvement pour générer les prochains niveaux
+        loc = move(loc, move);
+    }
 }
 
 void printTree(t_treeNode *node, int level) {
@@ -33,6 +59,19 @@ void printTree(t_treeNode *node, int level) {
     }
 }
 
+int calculateMoveCost(t_localisation loc, t_move move, t_map map) {
+    // Simuler la nouvelle localisation après le mouvement
+    t_localisation new_loc = move(loc, move);
+
+    // Vérifier si la nouvelle position est valide
+    if (!isValidLocalisation(new_loc.pos, map.x_max, map.y_max) ||
+        map.soils[new_loc.pos.y][new_loc.pos.x] == CREVASSE) {
+        return COST_UNDEF; // Mouvement invalide
+    }
+
+    // Retourner le coût du sol correspondant
+    return _soil_cost[map.soils[new_loc.pos.y][new_loc.pos.x]];
+}
 
 /*void freeTree(t_treeNode *root) {
     if (!root) return;
@@ -42,4 +81,3 @@ void printTree(t_treeNode *node, int level) {
     free(root->children);
     free(root);
 }*/
-
