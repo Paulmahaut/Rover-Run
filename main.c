e6c428504c57f2e3ce3b2bcbe3a1f302405323c2
Paulmahaut <paul.mahaut@gmail.com>
2024-11-19 09:13:34 +0100

    .

--- main.c
+++ main.c
@@ -22,15 +22,15 @@ int main() {
         }
         printf("\n");
     }
-    // printf the costs, aligned left 5 digits
-    for (int i = 0; i < map.y_max; i++)
+    /* printf the costs, aligned left 5 digits
+  for (int i = 0; i < map.y_max; i++)
     {
         for (int j = 0; j < map.x_max; j++)
         {
             printf("%-5d ", map.costs[i][j]);
         }
         printf("\n");
-    }
+    } */
     displayMap(map);
     return 0;
 }
